d2b1ac50be2f4005226e37cc4c5ae6de1710ad0b
blackzarifa <rafaelirades@gmail.com>
2025-04-30 18:41:01 -0300

    feat Read function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	DB_URL          string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+	configPath      string `json:"-"`
+}
+
+func Read(filename string) (*Config, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	configPath := filepath.Join(homeDir, filename)
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+	config.configPath = configPath
+
+	return &config, nil
+}
