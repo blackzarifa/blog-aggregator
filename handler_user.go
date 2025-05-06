429538047fe8157d391a89a38cac092c0053641f
blackzarifa <rafaelirades@gmail.com>
2025-05-06 20:34:20 -0300

    moving handleRegister func

--- handler_user.go
+++ handler_user.go
@@ -3,7 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
+	"gator/internal/database"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
 )
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	now := time.Now()
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: now,
+			UpdatedAt: now,
+			Name:      name,
+		},
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to register user: %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("User %s created successfully!\n", name)
+	log.Printf("User created: %+v\n", user)
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
