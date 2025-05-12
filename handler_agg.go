65f0f435346d8d3938a34e6f22247bac1cf02cce
blackzarifa <rafaelirades@gmail.com>
2025-05-12 18:57:40 -0300

    refac rss stuff to a single file

--- handler_agg.go
+++ handler_agg.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"gator/internal/rss"
 )
 
 func handlerAgg(s *state, cmd command) error {
 	ctx := context.Background()
 	feedURL := "https://www.wagslane.dev/index.xml"
 
-	feed, err := rss.FetchFeed(ctx, feedURL)
+	feed, err := fetchFeed(ctx, feedURL)
 	if err != nil {
 		return fmt.Errorf("error fetching feed: %w", err)
 	}
