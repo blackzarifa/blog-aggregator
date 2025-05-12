ac33c2fcd8aa814ce397de7f67a20366a77d77ac
blackzarifa <rafaelirades@gmail.com>
2025-05-12 18:44:52 -0300

    feat agg command

--- handler_user.go
+++ handler_user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"gator/internal/database"
+	"gator/internal/rss"
 	"log"
 	"time"
 
@@ -70,6 +71,19 @@ func handlerReset(s *state, cmd command) error {
 	}
 
 	fmt.Println("Deleted all users successfully!")
+	return nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	ctx := context.Background()
+	feedURL := "https://www.wagslane.dev/index.xml"
+	
+	feed, err := rss.FetchFeed(ctx, feedURL)
+	if err != nil {
+		return fmt.Errorf("error fetching feed: %w", err)
+	}
+	
+	fmt.Printf("%+v\n", feed)
 	return nil
 }
 
