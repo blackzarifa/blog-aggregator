92ca18d14d9c54d391d4ed9e98492ce08aa9186e
blackzarifa <rafaelirades@gmail.com>
2025-05-13 16:59:38 -0300

    feeds table creation and delete

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE feeds (
+    id uuid primary key,
+    created_at timestamp not null,
+    updated_at timestamp not null,
+    name text unique not null
+    url text unique not null
+    user_id uuid references users(id) on delete cascade,
+);
+
+-- +goose Down
+DROP TABLE feeds;
