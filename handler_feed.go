04a6a726f3838bc0289c4fd4c15314e20ac94f5e
blackzarifa <rafaelirades@gmail.com>
2025-05-14 17:19:35 -0300

    refac add feed

--- handler_feed.go
+++ handler_feed.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"fmt"
 	"gator/internal/database"
-	"log"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 func handlerAddFeed(s *state, cmd command) error {
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
 	if len(cmd.Args) != 2 {
 		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
 	}
@@ -18,33 +22,34 @@ func handlerAddFeed(s *state, cmd command) error {
 	name := cmd.Args[0]
 	url := cmd.Args[1]
 
-	if s.cfg.CurrentUserName == "" {
-		return fmt.Errorf("no user is logged in")
-	}
-
-	ctx := context.Background()
-	user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
-	if err != nil {
-		return fmt.Errorf("error getting current user: %w", err)
-	}
-
-	now := time.Now()
 	feed, err := s.db.CreateFeed(
-		ctx,
+		context.Background(),
 		database.CreateFeedParams{
 			ID:        uuid.New(),
-			CreatedAt: now,
-			UpdatedAt: now,
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			UserID:    user.ID,
 			Name:      name,
 			Url:       url,
-			UserID:    user.ID,
 		},
 	)
-
 	if err != nil {
-		return fmt.Errorf("failed to create feed: %w", err)
+		return fmt.Errorf("couldn't create feed: %w", err)
 	}
 
-	log.Printf("Feed created: %+v\n", feed)
+	fmt.Println("Feed created successfully:")
+	printFeed(feed)
+	fmt.Println()
+	fmt.Println("=====================================")
+
 	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* UserID:        %s\n", feed.UserID)
 }
